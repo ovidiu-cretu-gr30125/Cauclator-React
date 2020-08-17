a67e16ccacf0fe83c6065411476a001a7cf2294e
ovidiu-cretu-gr30125 <cretuovidiudaniel@yahoo.com>
2020-08-17 20:15:14 +0300

    Updates

---
+++ precache-manifest.3239e82284e2fad1ee274e1f9f113b21.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33e885e46fd951397d4c04fe5e0e21c2",
+    "url": "/Cauclator-React/index.html"
+  },
+  {
+    "revision": "f299474f48895ca2d098",
+    "url": "/Cauclator-React/static/css/main.eb17df18.chunk.css"
+  },
+  {
+    "revision": "114bbaea14242877659e",
+    "url": "/Cauclator-React/static/js/2.6912228e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Cauclator-React/static/js/2.6912228e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f299474f48895ca2d098",
+    "url": "/Cauclator-React/static/js/main.d01e882a.chunk.js"
+  },
+  {
+    "revision": "de7a0885aebf1bf9ee49",
+    "url": "/Cauclator-React/static/js/runtime-main.738644a2.js"
+  },
+  {
+    "revision": "dd857e7b06acf30c32e43350fcd82ca3",
+    "url": "/Cauclator-React/static/media/ClipartKey_2783339.dd857e7b.png"
+  },
+  {
+    "revision": "c831d3934deff2c1ca0c3d642eb39ba6",
+    "url": "/Cauclator-React/static/media/PngItem_5331595.c831d393.png"
+  },
+  {
+    "revision": "c32d16ecc826e0d3ffd2fc97fe70afa3",
+    "url": "/Cauclator-React/static/media/pngwing.com.c32d16ec.png"
+  },
+  {
+    "revision": "2e770dc7ec2d7fcfb8e62378cc655cea",
+    "url": "/Cauclator-React/static/media/unnamed.2e770dc7.png"
+  }
+]);
